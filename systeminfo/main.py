852f520d874e40103854772c923b798327adb8ed
Aoife OSullivan <aoifeosullivan19@gmail.com>
2018-02-22 11:02:00 +0000

    sixth commit

--- systeminfo/main.py
+++ systeminfo/main.py
@@ -1,10 +1,8 @@
 import shutil
 import platform
 
-def get_platform():
-	print("The platform is", platform.platform()) 
-	total, used, free = shutil.disk_usage(__file__)
-	print ('total, used, free: ', total, used, free)
-	return
+def get_platform():	
+	x="The platform is ", platform.platform()
+	return x
 if __name__ == '__main__':
 	get_platform()
