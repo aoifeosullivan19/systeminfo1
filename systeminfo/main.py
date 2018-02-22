d4bc5fc1b50a281d1edf4b0bffcf2c28e4be87ac
Aoife OSullivan <aoifeosullivan19@gmail.com>
2018-02-22 19:45:33 +0000

    another update

--- systeminfo/main.py
+++ systeminfo/main.py
@@ -2,7 +2,7 @@ import shutil
 import platform
 
 def get_platform():	
-	x="The platform is ", platform.platform()
+	x="The platform is ", platform.platform
 	return x
 if __name__ == '__main__':
 	get_platform()
